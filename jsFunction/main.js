053fc5931aa013aa40a4d734b8eda80692e8661b
Aram Abbasi <aramabbasi@yahoo.com>
2018-05-02 09:11:55 +0300

    Client side updated

--- jsFunction/main.js
+++ jsFunction/main.js
@@ -1,7 +1,33 @@
 let system_usrId = 0;
 let system_usrname = '';
 let user_has_order = 0;
-let isAdmin=1;
+let isAdmin = 1;
+function openadminNav() {
+    document.getElementById("bgdiv").style.display = "none";
+    document.getElementById("adminNav").style.width = "100%";
+    adminProduct();
+}
+
+function closeadminNav() {
+    document.getElementById("bgdiv").style.display = "block";
+
+    document.getElementById("adminNav").style.width = "0%";
+}
+function openNav() {
+    if (user_has_order > 0) {
+
+        document.getElementById("bgdiv").style.display = "none";
+        document.getElementById("myNav").style.width = "100%";
+        adminShoppingCart();
+    }
+
+}
+
+function closeNav() {
+    document.getElementById("bgdiv").style.display = "block";
+    document.getElementById("myNav").style.width = "0%";
+}
+
 function openModal(number) {
     var modal;
     var btn;
@@ -28,7 +54,11 @@ function openModal(number) {
     }
 
 }
-
+function seeallproduct() {
+    document.getElementById("forchangesection").style.display = "none";
+    document.getElementById("newproductsection").style.display = "block";
+    GetProducts(1);
+}
 function BaseUrl() {
     //return 'https://onlinetoy.azurewebsites.net/';
     //return 'http://localhost/OnlineShoppingProject/';
@@ -60,56 +90,128 @@ function showDataJsn(ctlName, resName, prmName, prmVal, fncName) {
 //Admin Functions
 function adminShoppingCart() {
     if (user_has_order > 0) {
-
-        document.getElementById("results_dyn").innerHTML = "";
-        document.getElementById("AdminPageHeader").innerHTML = "Shopping Cart";
-        document.getElementById("AdminPageContent").style.width = "100%";
-        document.getElementById("AdminPageContent").style.height = "100%";
-        modal = document.getElementById('AdminPage');
-
-        modal.classList.add("overlay");
-
-        span = document.getElementById('adminclose');
-        modal.style.display = "block";
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
         showDataJsn('Orders_ctl', 'userorders', 'SystemUsersId', userid(), ShoppingcartShow);
     }
 }
-
+function adminProduct() {
+    showDataJsn('Products_ctl', 'products', null, null, ProductShow);
+}
 function adminPriceCat() {
-    document.getElementById("results_dyn").innerHTML = "";
-    document.getElementById("AdminPageHeader").innerHTML = "Price Category";
-    document.getElementById("AdminPageContent").style.width = "50%";
-    document.getElementById("AdminPageContent").style.height = "60%";
-    modal = document.getElementById('AdminPage');
-    modal.classList.add("overlay");
-
-    span = document.getElementById('adminclose');
-    modal.style.display = "block";
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
     showDataJsn('Pricecategory_ctl', 'pricecategory', null, null, PriceCatShow);
 }
 function adminProductCat() {
-    document.getElementById("results_dyn").innerHTML = "";
-    document.getElementById("AdminPageHeader").innerHTML = "Product Category";
-    document.getElementById("AdminPageContent").style.width = "50%";
-    document.getElementById("AdminPageContent").style.height = "70%";
-    modal = document.getElementById('AdminPage');
-
-    span = document.getElementById('adminclose');
-    modal.style.display = "block";
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
     showDataJsn('Productscategory_ctl', 'productscategory', null, null, ProdCatShow);
+
+}
+function adminDiscount() {
+    showDataJsn('Discounts_ctl', 'discounts', null, null, DiscountShow);
 
 }
 
 //end of Admin Functions
+//Discount functions
+function DiscountShow(jsonData) {
+    var enbBut = '';
+    var elmtxt = '<button onclick=" PriceCatInsert() ">Add new</button> <br>' +
+        '<table " >' +
+        '<tr><td>' +
+        '<table style=" table-layout: fixed;" id="BaseShowTbl" class="table table-hover table-bordered table-striped " >' +
+        '<tr class="info">' +
+        '<th>Discount ID</th><th>Product name</th>' +
+        '<th>Start Date</th>' +
+        '<th>End Date</th>' +
+        '<th>Percent</th>' +
+        '<th>Edit</th><th>Delete</th>';
+    for (x in jsonData) {
+        //        enbBut = (jsonData[x].Chkuse > 0) ? 'disabled title="This Id is in use"' : 'enabled';
+        elmtxt +=
+            '<tr><td>' +
+            jsonData[x].DiscountsId +
+            '</td><td> ' +
+            jsonData[x].ProductName +
+            '</td><td> ' +
+            jsonData[x].DiscountStartDate +
+            '</td><td> ' +
+            jsonData[x].DiscountEndDate +
+            '</td><td> ' +
+
+            jsonData[x].DiscountPercent +
+            '</td>' +
+
+            '<td> <a href="javascript:DiscountEdit(' + jsonData[x].DiscountsId + '");"> <button class="btn btn-primary" ' +
+            '><span class="glyphicon glyphicson-edit"></button></a></td>' +
+            '<td> <a href="javascript:DiscountDelete(' + jsonData[x].DiscountsId + ');"> <button class="btn btn-primary" ' +
+
+            '><span class="glyphicon glyphicon-remove"></button></a></td>' +
+            '</tr>';
+    }
+    elmtxt += '</table></td> </tr></table>';
+    document.getElementById("adminpagedv").innerHTML = elmtxt;
+}
+
+//end of discount functions
+//Products function
+function SearchFunction() {
+    var input, filter, tbl, i;
+    tbl = document.getElementById("BaseShowTbl");
+    iRow = tbl.rows.length;
+    input = document.getElementById("SearchInput");
+    filter = input.value.toUpperCase();
+
+
+    for (i = 1; i < iRow; i++) {
+        a = tbl.rows[i].cells[1];
+
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            tbl.rows[i].style.display = "";
+        } else {
+            tbl.rows[i].style.display = "none";
+
+        }
+    }
+}
+function ProductShow(jsonData) {
+    var elmtxt = '<button onclick=" ProductInsert() ">Add new</button> <br>' +
+        '<input type="text" class"SearchInput" id="SearchInput" onkeyup="SearchFunction()" placeholder="Search for product names.." title="Type in a name of product">' +
+        '<table id="BaseShowTbl" class="table table-hover table-bordered table table-striped">' +
+        '<tr class="info">' +
+        '<th>Products ID</th>' +
+        '<th>Product Name</th> ' +
+        '<th>Quantity</th> ' +
+        '<th>Order Point</th> ' +
+        '<th>Category</th> ' +
+        '<th>Status</th> ' +
+        '<th>Price</th> ' +
+        '<th>Disscounted Price</th> ' +
+        '<th>Edit</th><th>Delete</th>';
+    for (x in jsonData) {
+        elmtxt +=
+            '<tr><td>' +
+            jsonData[x].ProductsId +
+            '</td><td> ' +
+            jsonData[x].ProductName +
+            '</td><td> ' +
+            jsonData[x].ProductQuantity +
+            '</td><td> ' +
+            jsonData[x].ProductOrderPoint +
+            '</td><td> ' +
+            jsonData[x].PrdCatDescription +
+            '</td><td> ' +
+            jsonData[x].ProductStatus +
+            '</td><td> ' +
+            jsonData[x].ProdFinalPrice +
+            '</td><td> ' +
+            jsonData[x].DiscountPrice +
+            '</td>' +
+            '<td> <a href="javascript:PriceCatEdit(' + jsonData[x].PriceCategoryId + ');"> <button class="btn btn-primary"><span class="glyphicon glyphicon-edit"></button></a></td>' +
+            '<td> <a href="javascript:PriceCatDelete(' + jsonData[x].PriceCategoryId + ');"> <button class="btn btn-primary"><span class="glyphicon glyphicon-remove"></button></a></td>' +
+            '</tr>';
+    }
+    elmtxt += '</table>';
+    document.getElementById("adminpagedv").innerHTML = elmtxt;
+}
+
+//end of products function
 //Product category Functions
 function ProdCatShow(jsonData) {
     var enbBut = '';
@@ -136,7 +238,7 @@ function ProdCatShow(jsonData) {
             '</tr>';
     }
     elmtxt += '</table></td> </tr></table>';
-    document.getElementById("results_dyn").innerHTML = elmtxt;
+    document.getElementById("adminpagedv").innerHTML = elmtxt;
 }
 
 function ProdCatEdit(ProductsCategoryId, PrdCatDescription) {
@@ -180,7 +282,6 @@ function ProdCatDelete(ProductsCategoryId) {
         xhttp.open('GET', url, true);
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
-                alert(xhttp.responseText);
                 adminProductCat();
             }
         };
@@ -220,8 +321,10 @@ function ProdcatSave(mtdflag) {
 
 function PriceCatShow(jsonData) {
     var elmtxt = '<button onclick=" PriceCatInsert() ">Add new</button> <br>' +
-        '<table id="BaseShowTbl" class="table table-hover table-bordered table ">' +
+
+        '<table id="BaseShowTbl" class="table table-hover table-bordered table-striped ">' +
         '<tr class="info">' +
+
         '<th>Price Category ID</th><th>Perecent</th><th>Edit</th><th>Delete</th>';
     for (x in jsonData) {
         elmtxt +=
@@ -235,7 +338,7 @@ function PriceCatShow(jsonData) {
             '</tr>';
     }
     elmtxt += '</table>';
-    document.getElementById("results_dyn").innerHTML = elmtxt;
+    document.getElementById("adminpagedv").innerHTML = elmtxt;
 }
 
 function PriceCatEdit(PriceCategoryId, PriceCatPerecent) {
@@ -315,8 +418,12 @@ function PricecatSave(mtdflag) {
 
 }
 // End of Price category functions
-function GetProducts() {
-    var geturl = BaseUrl() + 'index.php/Products_ctl/products3';
+function GetProducts(flgstat) {
+    if (flgstat == 0)
+        var geturl = BaseUrl() + 'index.php/Products_ctl/products3';
+    else
+        var geturl = BaseUrl() + 'index.php/Products_ctl/products';
+
     var imgurl = BaseUrl() + '/images/';
     var xhttp = new XMLHttpRequest();
     xhttp.open('GET', geturl, true);
@@ -356,99 +463,128 @@ function GetProducts() {
                         ' </figure>';
                 }
             }
-            document.getElementById('products').innerHTML = data;
+            if (flgstat == 0)
+                document.getElementById('products').innerHTML = data;
+            else
+                document.getElementById('productsallshow').innerHTML = data;
+
 
         }
     };
     xhttp.send();
 }
 function addtcocart(ProductsId, UserId) {
-    // alert('order not added');
-    var geturl = BaseUrl() + 'index.php/Orders_ctl/orderscart';
-    var orderdata = {};
-    orderdata.ProductsId = parseInt(ProductsId);
-    orderdata.SystemUsersId = parseInt(UserId);
 
-    var jsonData = JSON.stringify(orderdata);
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('POST', geturl, true);
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4 && xhttp.status == 201) {
-            alert('order already added');
-        }
-    };
-    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhttp.send(jsonData);
+    // alert('order not added');
+    if (system_usrId > 0) {
+
+        var geturl = BaseUrl() + 'index.php/Orders_ctl/orderscart';
+        var orderdata = {};
+        orderdata.ProductsId = parseInt(ProductsId);
+        orderdata.SystemUsersId = system_usrId;
+
+        var jsonData = JSON.stringify(orderdata);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('POST', geturl, true);
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 201) {
+                alert('Product added to your cart');
+                user_has_order = 1;
+                setnavstat(0);
+
+            }
+        };
+        xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhttp.send(jsonData);
+    }
+    else {
+        alert('Pleas login / register First');
+    }
 }
 //Shopping cart
+function calcprice(itmprc, itmcounter) {
+    let quaid = "Itemqua_" + itmcounter;
+    let prodprcid = "Productsprice_" + itmcounter;
+    let oldprc = document.getElementById(prodprcid).innerHTML;
+    let totprc = document.getElementById("Cartsubtotal").innerHTML;
+
+    let itemqua = document.getElementById(quaid).value * itmprc;
+    document.getElementById(prodprcid).innerHTML = itemqua;
+    document.getElementById("Cartsubtotal").innerHTML = (totprc - oldprc) + itemqua;
+
+    //document.getElementById(prodprcid).innerHTML*itmprc;
+}
+function deletecart(orderid) {
+    if (confirm("Are you sure to delete this order??")) {
+        let url = BaseUrl() + 'index.php/Orders_ctl/ordersdel/OrdersId/' + orderid.toString();
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url, true);
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                adminShoppingCart();
+            }
+        };
+        //  xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        //        xhttp.withCredentials = true;
+        xhttp.send();
+    }
+
+}
+function UpdateOrder() {
+    if (confirm("Are you sure for checkout?")) {
+
+        var url = BaseUrl() + 'index.php/Orders_ctl/ordersupd?SystemUsersId=' + system_usrId.toString();
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url, true);
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 201) {
+                user_has_order = 0;
+                closeNav();
+                setnavstat(0);
+
+            }
+        };
+
+        xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhttp.send();
+    }
+
+}
 function ShoppingcartShow(jsonData) {
     var imgurl = BaseUrl() + '/images/';
-
+    var counter = 0;
     var totprc = 0
-    var data =
-        '<div id="myNav" class="overlay">'
-    '<div class="ShoppingCart">' +
-        '<div class="Namecolumns">' +
-        '<label class="Itemimage">Image</label>' +
-        '<label class="Itemdescription">Description of Products</label>' +
-        '<label class="Itemprice">Price</label>' +
-        '<label class="Itemquantity">Quantity</label>' +
-        '<label class="Itemremove">Remove</label>' +
-        '<label class="Productsprice">Total</label>' +
-        '</div>';
+    var data = '';
 
 
     for (x in jsonData) {
-        totprc += jsonData[x].OrderPrice;
+        counter++;
+        totprc += jsonData[x].OrderPrice * jsonData[x].OrderQuantity;
         data +=
 
             '<div class="Item">' +
             '<div class="Itemimage">' +
-            '<img src="' + imgurl + jsonData[x].ProductPicture + '" height="200" width="200">' +
+            '<img src="' + imgurl + jsonData[x].ProductPicture + '" height="100" width="100">' +
             '</div>' +
             '<div class="Itemdescription">' +
             '<div class="Itemname">' + jsonData[x].ProductName + '</div>' +
             '<p class="Itemdetails">' + jsonData[x].ProductDesc + '</p>' +
             '</div>' +
-            '<div class="Itemprice">' + jsonData[x].OrderPrice + '</div>' +
+            '<div  class="Itemprice">' + jsonData[x].OrderPrice + '</div>' +
             '<div class="Itemquantity">' +
-            '<input type="number" value="1" min="1">' +
+            '<input id="Itemqua_' + counter + '" type="number"  value="' + jsonData[x].OrderQuantity + '" min="1" oninput="calcprice(' + jsonData[x].OrderPrice + ',' + counter + ')">' +
             '</div>' +
             '<div class="Itemremove">' +
             '<button class="Removeitem" onclick="deletecart(' + jsonData[x].OrdersId + ')">' +
             'Remove' +
             '</button>' +
             '</div>' +
-            '<div class="Productsprice">' + jsonData[x].OrderPrice * jsonData[x].OrderQuantity + '</div>' +
+            '<div id="Productsprice_' + counter + '" class="Productsprice">' + jsonData[x].OrderPrice * jsonData[x].OrderQuantity + '</div>' +
             '</div>';
 
     }
-    data +=
-        ' <div class="Total">' +
-        '<div class="Itemtotal">' +
-        '<label>Subtotal</label>' +
-        ' <div class="Totalvalue" id="Cartsubtotal"></div>' +
-        ' </div>' +
-        '<div class="Itemtotal">' +
-        '  <label>Tax (5%)</label>' +
-        ' <div class="Totalvalue" id="Carttax">3</div>' +
-        ' </div>' +
-        ' <div class="Itemtotal">' +
-        ' <label>Shipping</label>' +
-        ' <div class="Totalvalue" id="Cartshipping">10.00</div>' +
-        ' </div>' +
-        ' <div class="Itemtotal Itemtotal-Total">' +
-        ' <label>Grand Total</label>' +
-        ' <div class="Totalvalue" id="Carttotal">73</div>' +
-        '</div>' +
-        ' </div>' +
-
-        '<button class="Checkout">Checkout</button>' +
-        ' </div>' +
-
-        ' </div>';
-
-    document.getElementById("results_dyn").innerHTML = data;
+    document.getElementById("customershopcart").innerHTML = data;
+    document.getElementById("Cartsubtotal").innerHTML = totprc;
 
 }
 //End of shopping cart  
@@ -471,7 +607,7 @@ function loginuser(usreml, urspass, srcflg) {
             system_usrId = parseInt(jsonResponse["userId"]);
             system_usrname = jsonResponse["username"];
             user_has_order = jsonResponse["hasorder"];
-            isAdmin= jsonResponse["isAdmin"];
+            isAdmin = jsonResponse["isAdmin"];
             setnavstat(0);
         }
     };
@@ -487,7 +623,7 @@ function logoutuser() {
             system_usrId = 0;
             system_usrname = '';
             user_has_order = 0;
-            isAdmin=1;
+            isAdmin = 1;
             setnavstat(0);
         }
     };
@@ -496,12 +632,19 @@ function logoutuser() {
 }
 
 function setnavstat(flgfirst) {
+    if (flgfirst == 1) {
+        system_usrId = 0;
+        system_usrname = '';
+        user_has_order = 0;
+        isAdmin = 1;
+
+    }
     if (system_usrId > 0) {
-        if(isAdmin==0){
+        if (isAdmin == 0) {
             document.getElementById("navbarDropdown").style.display = "block";
-            
+
         }
-        else{
+        else {
             document.getElementById("navbarDropdown").style.display = "none";
 
         }
@@ -512,20 +655,28 @@ function setnavstat(flgfirst) {
         if (user_has_order == 0) {
 
             document.getElementById('shopcart').style.color = "black";
+            document.getElementById('shopcart').disabled = true;
+
         }
         else {
             document.getElementById('shopcart').style.color = "#F18000";
+            document.getElementById('shopcart').disabled = false;;
+
 
         }
     }
     else {
+        document.getElementById("forchangesection").style.display = "block";
+        document.getElementById("newproductsection").style.display = "none";
+    
         document.getElementById("navbarDropdown").style.display = "none";
         document.getElementById("LoginB").style.display = "block";
         document.getElementById("RegisterB").innerText = "Register";
         document.getElementById('hiusrname').innerText = '';
+        document.getElementById('shopcart').disabled = true;
         document.getElementById('shopcart').style.color = "black";
         if (flgfirst == 1)
-            GetProducts();
+            GetProducts(0);
     }
 
 }
